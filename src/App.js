fc0461e515b83750ada647ddf97140085a8f6764
BexCdz <beckypearce92@hotmail.com>
2026-04-01 10:57:51 +0200

    Created the HTML structure for weather app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import React from "react";
-
+import Weather from "./Weather"
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-   <h1>  Weather App</h1>
+   
+   <Weather/>
      <footer>This project was coded by Becky Pearce and is {""}  
      <a href= "https://github.com/BexCdz/react-weather-app" >open-sourced on GitHub </a>
      </footer>
